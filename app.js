2bcffd7027e51f0884b964ca4f2b1458650bc8fa
Zunayed Mahfuz <zunayedmahfuz@Zunayeds-MacBook-Air.local>
2020-07-10 00:15:35 -0400

    issue with Git

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const DOMAIN = 'http://www.omdbapi.com/';
 const API_KEY = /* Your API key goes here */
+
 const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`;
 
 
+const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`;
